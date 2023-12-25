8e3889cadbea116fe8c79bfbbb2a5aea205143ae
daredakke <77235881+daredakke@users.noreply.github.com>
2023-12-25 20:56:11 +0000

    Update decision_panel.gd
    Make the height calculation a little cleaner.

--- scripts/decision_panel.gd
+++ scripts/decision_panel.gd
@@ -13,9 +13,10 @@ func _ready() -> void:
 
 func set_height_and_position() -> void:
 	var child_count: int = decision_v_box.get_child_count()
-	var separation = decision_v_box.get_theme_constant("separation")
+	var separation: int = decision_v_box.get_theme_constant("separation")
+	var gutters: int = child_count - 1
 	
-	var height: int = (BUTTON_HEIGHT * child_count) + (MARGIN * 2) + (separation * (child_count - 1))
+	var height: int = (BUTTON_HEIGHT * child_count) + (MARGIN * 2) + (separation * gutters)
 	
 	self.size.y = height
 	self.position.y = (get_viewport_rect().size.y * 0.5) - (height * 0.5)
